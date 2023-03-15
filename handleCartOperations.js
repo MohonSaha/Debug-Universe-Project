888e65a2bc5ce5ee2f1d67c2a7ec9f43c65fd43b
MohonSaha <practicewithmohon@gmail.com>
2023-03-15 19:20:45 +0600

    fix all bug

--- handleCartOperations.js
+++ handleCartOperations.js
@@ -3,18 +3,25 @@ const addToCart = async (id) => {
   const result = await data.json();
   const { name, summary, property_type, images,number_of_reviews,price,_id} = result.find((item) => item._id == id);
   const cartItems=getItemsFromStorage()
+
+  if(cartItems.find(item => item._id == id)){
+    return;
+  }
+  else{
+    cartItems.push({ name, summary, property_type, images,number_of_reviews,price,_id});
+    localStorage.setItem('savedCart', JSON.stringify(cartItems))
+    //const cartItemsContainer = document.getElementById("cart-items");
+
+  }
   
-  
-  cartItems.push({ name, summary, property_type, images,number_of_reviews,price,_id});
-  localStorage.setItem('saved-Cart', JSON.stringify(cartItems))
-  //const cartItemsContainer = document.getElementById("cart-items");
+  displayCartItems()
 };
 
 const getItemsFromStorage = () => {
   let itemsArray = [];
   const cartItems = localStorage.getItem("savedCart");
   if (cartItems) {
-    itemsArray = (cartItems);
+    itemsArray = JSON.parse(cartItems);
   }
   return itemsArray;
 };
@@ -23,17 +30,17 @@ const getItemsFromStorage = () => {
 
 const displayCartItems=()=>{
     const cartItemsContainer = document.getElementById("cart-items");
-    const cartItems=getItemsFromStorage()
+    const cartItems = getItemsFromStorage();
     cartItemsContainer.innerHTML=""
     cartItems?.forEach((item)=>{
-        const { name, property_type, images,number_of_reviews,price,_id}=item
+        const { name, property_type, images,number_of_reviews,price,_id}= item
         cartItemsContainer.innerHTML += `
         <tr>
         <th scope="row">${name.slice(0,26)}</th>
         <td><span> <i onclick='deleteItemFromCart(${_id})' class="mx-2 bi bi-trash3 text-danger"></i>
         </span> 
         <span> 
-        <i class="text-success bi bi-credit-card-fill" onclick='handlePaymentInfo(${_id})' data-bs-toggle="modal" data-bs-target="#paymenModal" ></i> 
+        <i class="text-success bi bi-credit-card-fill" onclick='handlePaymentInfo(${_id})' data-bs-toggle="modal" data-bs-target="#paymentModal" ></i> 
         </span></td>
        
         </tr>
@@ -46,7 +53,7 @@ displayCartItems()
 
 const deleteItemFromCart=(id)=>{
     const cartItems=getItemsFromStorage()
-    const filteredItems=cartItems.filter((item)=>item._id==id)
+    const filteredItems=cartItems.filter((item)=>item._id != id)
     localStorage.setItem('savedCart', JSON.stringify(filteredItems))
     displayCartItems()
 }
